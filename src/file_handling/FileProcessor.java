973fe00f87b3e779c7106c4d61f5412ecc78ef80
Brandon Hinton <brandon.hinton@saabgroup.com>
2025-02-03 08:01:15 +0000

    removed csv processor as no longer needed, added interfaces to file processing classes

--- src/file_handling/FileProcessor.java
+++ src/file_handling/FileProcessor.java
@@ -1,16 +1,20 @@
 package file_handling;
+
 import java.io.IOException;
+
+import file_handling.interfaces.IFileProcessor;
 
 /**
  * Abstract base class for processing different types of files.
  * Implements template method pattern for file processing workflow.
  */
-public abstract class FileProcessor
+public abstract class FileProcessor implements IFileProcessor
 {
     protected String filePath;
 
     /**
      * Constructs a FileProcessor with the specified file path.
+     *
      * @param filePath Path to the file to be processed
      */
     public FileProcessor(String filePath)
@@ -21,25 +25,29 @@ public abstract class FileProcessor
     /**
      * Template method that defines the file processing algorithm.
      * Subclasses should not override this method.
+     *
      * @throws IOException if file operations fail
      */
     public abstract void processFile() throws IOException;
 
     /**
      * Validates if the file meets the required format and existence criteria.
+     *
      * @throws IllegalArgumentException if file validation fails
      */
-    protected abstract void validateFile();
+    public abstract void validateFile();
 
     /**
      * Reads the contents of the file into memory.
+     *
      * @throws IOException if file reading operations fail
      */
-    protected abstract void readFile() throws IOException;
+    public abstract void readFile() throws IOException;
 
     /**
      * Parses the file content into the appropriate data structure.
+     *
      * @throws IOException if parsing operations fail
      */
-    protected abstract void parseContent() throws IOException;
+    public abstract void parseContent() throws IOException;
 }
